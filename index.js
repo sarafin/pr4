4847a6972fb74b1dcadadb197e7255d029d3ea8e
Olya <olka_krishtal@mail.ru>
2016-06-04 12:47:04 +0000

    :done

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ function show(state){
 					document.getElementById('window').style.display = state;			
 					document.getElementById('wrap').style.display = state; 			
 			}
+
+$('#btn-send').click(function(e) {
+  // Please set your email in url field
+  $.ajax({
+      url: "https://formspree.io/olka_krishtal@mail.ru",  
+      method: "POST",
+      error: function () {
+        $('#container').html('<h1>Error!</h1>')
+      },
+      data: {
+             email: enterEmail.value,
+             name: enterName.value
+             },
+      dataType: "json"
+  }).done(function() {
+     $('#container').html('<h1>Thank you!</h1>')
+  } );
+  e.preventDefault();
+});
